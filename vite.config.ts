3455f7df2eb2a10f27d72eabd66b9c9573c2d977
Daniele <daniele@anothertomorrow.io>
2025-09-19 11:22:35 +0200

    git: add github actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: '/daniele-three-ways/',
     server: {
         host: '::',
         port: 8080,
