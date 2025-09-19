18171bd2d6a63e33a65d7723324113f424eab9d8
Daniele <daniele@anothertomorrow.io>
2025-09-19 11:40:14 +0200

    bug: changed the vite config to match the custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: '/daniele-three-ways/',
-    server: {
-        host: '::',
-        port: 8080,
-    },
+    base: '/',
     plugins: [react()],
     resolve: {
         alias: {
